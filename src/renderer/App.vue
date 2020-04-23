a8b2869aac156d72cb0515b048e26cc350a8cd5e
codeyung <codeyung@locahost>
2020-04-23 22:38:40 +0800

    first

---
+++ src/renderer/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <el-button type="danger">测试按钮</el-button>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'myapp-vue'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
